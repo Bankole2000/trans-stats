9a71f2ac37b8600381e1731bd19a05e89aee65dc
Bankole Esan <techybanky@gmail.com>
2021-08-18 21:39:32 +0100

    redis store rate-limiting

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const rateLimit = require("express-rate-limit");
+const RedisStore = require("rate-limit-redis");
+const Redis = require('redis');
+const redisClient = Redis.createClient();
+
+const apiLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  store: new RedisStore({
+    client: redisClient,
+  }),
+  max: 120,
+  message:
+    "Too many accounts created from this IP, please try again after a minute"
+});
 
 app.use(express.json());
 
@@ -14,7 +28,7 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: "Welcome to the Transaction Statistics API", endPoints })
 })
 
-app.post('/transactions', transactionsController.createTransaction)
+app.post('/transactions', apiLimiter, transactionsController.createTransaction)
 
 app.get("/statistics", transactionsController.getStatistics)
 
